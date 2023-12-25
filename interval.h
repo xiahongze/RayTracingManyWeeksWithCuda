367a865e237a0e2493bdb672316b3b559d256d38
Hongze Xia <hongze.x@gmail.com>
2023-12-25 22:34:11 +1100

    feat: inline most interval

--- interval.h
+++ interval.h
@@ -13,28 +13,28 @@ public:
 
     __host__ __device__ interval(float _min, float _max) : min(_min), max(_max) {}
 
-    __host__ __device__ float size() const
+    __host__ __device__ inline float size() const
     {
         return max - min;
     }
 
-    __host__ __device__ interval expand(float delta) const
+    __host__ __device__ inline interval expand(float delta) const
     {
         auto padding = delta / 2;
         return interval(min - padding, max + padding);
     }
 
-    __host__ __device__ bool contains(float x) const
+    __host__ __device__ inline bool contains(float x) const
     {
         return min <= x && x <= max;
     }
 
-    __host__ __device__ bool surrounds(float x) const
+    __host__ __device__ inline bool surrounds(float x) const
     {
         return min < x && x < max;
     }
 
-    __host__ __device__ float clamp(float x) const
+    __host__ __device__ inline float clamp(float x) const
     {
         if (x < min)
             return min;
