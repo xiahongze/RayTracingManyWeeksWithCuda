4278cb955e457b73d10d603cf1104ec3f085471d
Hongze Xia <hongze.x@gmail.com>
2023-12-26 21:37:57 +1100

    feat: match scatter in dielectric

--- material.h
+++ material.h
@@ -6,7 +6,12 @@ struct hit_record;
 #include "ray.h"
 #include "hitable.h"
 
-__device__ float schlick(float cosine, float ref_idx)
+__device__ inline float fmin_dev(float a, float b)
+{
+    return a < b ? a : b;
+}
+
+__device__ float reflectance(float cosine, float ref_idx)
 {
     // Use Schlick's approximation for reflectance.
     float r0 = (1.0f - ref_idx) / (1.0f + ref_idx);
@@ -14,18 +19,12 @@ __device__ float schlick(float cosine, float ref_idx)
     return r0 + (1.0f - r0) * pow((1.0f - cosine), 5.0f);
 }
 
-__device__ bool refract(const vec3 &v, const vec3 &n, float ni_over_nt, vec3 &refracted)
+__device__ vec3 refract(const vec3 &uv, const vec3 &n, float etai_over_etat)
 {
-    vec3 uv = unit_vector(v);
-    float dt = dot(uv, n);
-    float discriminant = 1.0f - ni_over_nt * ni_over_nt * (1 - dt * dt);
-    if (discriminant > 0)
-    {
-        refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
-        return true;
-    }
-    else
-        return false;
+    auto cos_theta = fmin_dev(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 __device__ vec3 reflect(const vec3 &v, const vec3 &n)
@@ -90,34 +89,22 @@ public:
                                     ray &scattered,
                                     curandState *local_rand_state) const
     {
-        vec3 outward_normal;
-        vec3 reflected = reflect(r_in.direction(), rec.normal);
-        float ni_over_nt;
         attenuation = vec3(1.0, 1.0, 1.0);
-        vec3 refracted;
-        float reflect_prob;
-        float cosine;
-        if (dot(r_in.direction(), rec.normal) > 0.0f)
-        {
-            outward_normal = -rec.normal;
-            ni_over_nt = ref_idx;
-            cosine = dot(r_in.direction(), rec.normal) / r_in.direction().length();
-            cosine = sqrt(1.0f - ref_idx * ref_idx * (1 - cosine * cosine));
-        }
+        float refraction_ratio = rec.front_face ? (1.0 / ref_idx) : ref_idx;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        float cos_theta = fmin_dev(dot(-unit_direction, rec.normal), 1.0);
+        float sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > curand_uniform(local_rand_state))
+            direction = reflect(unit_direction, rec.normal);
         else
-        {
-            outward_normal = rec.normal;
-            ni_over_nt = 1.0f / ref_idx;
-            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
-        }
-        if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
-            reflect_prob = schlick(cosine, ref_idx);
-        else
-            reflect_prob = 1.0f;
-        if (curand_uniform(local_rand_state) < reflect_prob)
-            scattered = ray(rec.p, reflected);
-        else
-            scattered = ray(rec.p, refracted);
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
         return true;
     }
 
