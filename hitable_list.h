20e83a7fc87a32d0db0d50e26636b03bdf245734
Hongze Xia <hongze.x@gmail.com>
2023-12-28 12:54:58 +1100

    refactor: hitable_list not a subclass of hitable (#9)
    - hitable_list not a subclass of hitable
    - rm warning like nvlink warning : Stack size for entry function '_Z6renderP4vec3iiiP6cameraPP7hitable' cannot be statically determined

--- hitable_list.h
+++ hitable_list.h
@@ -2,12 +2,13 @@
 
 #include "hitable.h"
 
-class hitable_list : public hitable
+class hitable_list
 {
 public:
     __device__ hitable_list();
     __device__ hitable_list(hitable **l, int n);
-    __device__ bool hit(const ray &r, const interval ray_t, hit_record &rec) const override;
+    // same as hitable::hit but not a subclass of hitable
+    __device__ bool hit(const ray &r, const interval ray_t, hit_record &rec) const;
     __device__ ~hitable_list();
 
     hitable **list;
