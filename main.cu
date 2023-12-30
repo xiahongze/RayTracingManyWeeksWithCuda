6dae5a8778232212aa746b5b8696987aaf1858f6
Hongze Xia <hongze.x@gmail.com>
2023-12-30 18:22:13 +1100

    refactor: get rid of hitable list

--- main.cu
+++ main.cu
@@ -1,7 +1,6 @@
 #include "bvh.h"
 #include "camera.h"
 #include "cmd_parser.h"
-#include "hitable_list.h"
 #include "image_utils.h"
 #include "interval.h"
 #include "material.h"
@@ -96,7 +95,7 @@ __global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, camera *d_camer
     d_fb[pixel_index] = col;
 }
 
-__global__ void create_world(bvh_node *d_bvh_nodes, hitable_list **d_world, hitable **d_list, camera *d_camera, int list_size, int nx, int ny, bool bounce, float bounce_pct)
+__global__ void create_world(bvh_node *d_bvh_nodes, hitable **d_list, camera *d_camera, int list_size, int nx, int ny, bool bounce, float bounce_pct)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -145,8 +144,6 @@ __global__ void create_world(bvh_node *d_bvh_nodes, hitable_list **d_world, hita
         // create bvh_nodes
         bvh_node::prefill_nodes(d_bvh_nodes, d_list, list_size);
 
-        *d_world = new hitable_list(d_list, list_size);
-
         *d_camera = camera();
         d_camera->lookfrom = vec3(13, 2, 3);
         d_camera->lookat = vec3(0, 0, 0);
@@ -159,11 +156,12 @@ __global__ void create_world(bvh_node *d_bvh_nodes, hitable_list **d_world, hita
     }
 }
 
-__global__ void free_world(hitable_list **d_world)
+__global__ void free_objects(hitable **d_list, int size)
 {
-    if (threadIdx.x > 0 || blockIdx.x > 0)
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= size)
         return;
-    delete *d_world;
+    delete d_list[idx];
 }
 
 int main(int argc, char **argv)
@@ -178,8 +176,6 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
     // make our world of hitables & the camera
-    hitable_list **d_world;
-    checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable_list *)));
     hitable **d_list;
     int list_size = 22 * 22 + 1 + 3;
 
@@ -191,12 +187,11 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaMalloc((void **)&d_list, list_size * sizeof(hitable *)));
     camera *d_camera;
     checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera)));
-    create_world<<<dim3(1, 1), dim3(22, 22)>>>(d_bvh_nodes, d_world, d_list, d_camera, list_size,
+    create_world<<<dim3(1, 1), dim3(22, 22)>>>(d_bvh_nodes, d_list, d_camera, list_size,
                                                cmd_opts.image_width, cmd_opts.image_height, cmd_opts.bounce, cmd_opts.bounce_pct);
     checkCudaErrors(cudaGetLastError());
     // copy bvh_nodes from device to host
     checkCudaErrors(cudaMemcpy(h_bvh_nodes, d_bvh_nodes, list_size * sizeof(bvh_node), cudaMemcpyDeviceToHost));
-    std::cout << "bvh_nodes copied to host\n";
     // build bvh tree on host
     int tree_height = bvh_node::build_tree(h_bvh_nodes, list_size);
 
@@ -216,15 +211,14 @@ int main(int argc, char **argv)
 
     stop = clock();
     double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-    std::cerr << "took " << timer_seconds << " seconds.\n";
+    std::clog << "took " << timer_seconds << " seconds.\n";
 
     // Output FB as Image, allocated with cudaMallocManaged can be directly accessed on host
     writeJPGImage(cmd_opts.output_file.c_str(), cmd_opts.image_width, cmd_opts.image_height, fb);
 
     // clean up
-    free_world<<<1, 1>>>(d_world);
+    free_objects<<<dim3(1), dim3(32)>>>(d_list, list_size);
     checkCudaErrors(cudaFree(d_camera));
-    checkCudaErrors(cudaFree(d_world));
     checkCudaErrors(cudaFree(d_list));
     checkCudaErrors(cudaFree(d_bvh_nodes));
     checkCudaErrors(cudaFree(fb));
