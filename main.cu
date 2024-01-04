a5f5d87b1aef9fe801ff807ed44180bccaef9cec
Hongze Xia <hongze.x@gmail.com>
2024-01-04 22:18:26 +1100

    feat: two_perlin_spheres

--- main.cu
+++ main.cu
@@ -39,6 +39,10 @@ int main(int argc, char **argv)
         earth(h_bvh_nodes, d_bvh_nodes, d_list, d_camera, list_size, tree_size,
               cmd_opts.image_width, cmd_opts.image_height);
         break;
+    case 2:
+        two_perlin_spheres(h_bvh_nodes, d_bvh_nodes, d_list, d_camera, list_size, tree_size,
+                           cmd_opts.image_width, cmd_opts.image_height);
+        break;
     default:
         exit(1);
     }
--- scenes-wk2.cu
+++ scenes-wk2.cu
@@ -13,7 +13,7 @@ __global__ void create_earth(bvh_node *d_bvh_nodes, hitable **d_list, camera *d_
     if ((i > 0) || (j > 0))
         return;
 
-    auto *earth_texture = new rtapp::image_texture(d_pixel_data, width, height, channels);
+    auto earth_texture = new rtapp::image_texture(d_pixel_data, width, height, channels);
     d_list[0] = new sphere(vec3(0, 0, 0), 2.0, new lambertian(earth_texture));
 
     // create bvh_nodes
@@ -51,4 +51,43 @@ void earth(bvh_node *&h_bvh_nodes, bvh_node *&d_bvh_nodes, hitable **&d_list, ca
                                              list_size, nx, ny);
 
     std::cout << "earth scene created" << std::endl;
+}
+
+__global__ void create_two_perlin_spheres(bvh_node *d_bvh_nodes, hitable **d_list, camera *d_camera,
+                                          int list_size, int nx, int ny)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i > 0) || (j > 0))
+        return;
+
+    auto perlin_texture = new rtapp::noise_texture(4.0);
+    d_list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(perlin_texture));
+    d_list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(perlin_texture));
+
+    // create bvh_nodes
+    bvh_node::prefill_nodes(d_bvh_nodes, d_list, list_size);
+
+    *d_camera = camera();
+    d_camera->lookfrom = vec3(13, 2, 5);
+    d_camera->lookat = vec3(0, 0, 0);
+    d_camera->vup = vec3(0, 1, 0);
+    d_camera->vfov = 20.0;
+    d_camera->image_width = nx;
+    d_camera->image_height = ny;
+    d_camera->defocus_angle = 0.0;
+    d_camera->initialize();
+}
+
+void two_perlin_spheres(bvh_node *&h_bvh_nodes, bvh_node *&d_bvh_nodes, hitable **&d_list, camera *&d_camera, int &list_size, int &tree_size, int nx, int ny)
+{
+    list_size = 2;
+    checkCudaErrors(cudaMalloc((void **)&d_list, list_size * sizeof(hitable *)));
+
+    tree_size = 2 * list_size;
+    h_bvh_nodes = new bvh_node[tree_size]; // binary tree
+    checkCudaErrors(cudaMalloc((void **)&d_bvh_nodes, tree_size * sizeof(bvh_node)));
+
+    create_two_perlin_spheres<<<dim3(1, 1), dim3(1, 1)>>>(d_bvh_nodes, d_list, d_camera,
+                                                          list_size, nx, ny);
 }
