09143c2dc2b4a87871f8579b815672419ed8ff19
Hongze Xia <hongze.x@gmail.com>
2024-01-21 16:29:01 +1100

    feat: render addes hitable_list *d_lights

--- render.cu
+++ render.cu
@@ -8,7 +8,7 @@
 // it was blowing up the stack, so we have to turn this into a
 // limited-depth loop instead.  Later code in the book limits to a max
 // depth of 50, so we adapt this a few chapters early on the GPU.
-__device__ vec3 get_ray_color_pixel(const int max_depth, const ray &r, bvh_node *d_bvh_nodes, vec3 &backgroound, curandState *local_rand_state)
+__device__ vec3 get_ray_color_pixel(const int max_depth, const ray &r, bvh_node *d_bvh_nodes, hitable_list *d_lights, vec3 &backgroound, curandState *local_rand_state)
 {
     ray cur_ray = r;
     vec3 cur_attenuation = vec3(1.0, 1.0, 1.0);
@@ -40,22 +40,36 @@ __device__ vec3 get_ray_color_pixel(const int max_depth, const ray &r, bvh_node 
         }
 
         // do sample
-        pdf *p;
+        pdf *p_cur;
         switch (srec.pdf_type_)
         {
         case pdf_type::COSINE:
-            p = &srec.cosine_pdf_;
+            p_cur = &srec.cosine_pdf_;
             break;
         case pdf_type::HITABLE:
-            p = &srec.hitable_pdf_;
+            p_cur = &srec.hitable_pdf_;
             break;
         case pdf_type::MIXTURE:
-            p = &srec.mixture_pdf_;
+            p_cur = &srec.mixture_pdf_;
             break;
         default:
-            p = &srec.sphere_pdf_;
+            p_cur = &srec.sphere_pdf_;
             break;
         }
+
+        pdf *p;
+        hitable_pdf pdf_light = hitable_pdf(d_lights, rec.p);
+        mixture_pdf p_light(&pdf_light, p_cur);
+
+        if (d_lights->length() > 0) // if there is light
+        {
+            p = &p_light;
+        }
+        else
+        {
+            p = p_cur;
+        }
+
         ray scattered = ray(rec.p, p->generate(local_rand_state), r.get_time());
         auto pdf_val = p->value(scattered.direction(), local_rand_state);
 
@@ -67,7 +81,7 @@ __device__ vec3 get_ray_color_pixel(const int max_depth, const ray &r, bvh_node 
     return final_color; // exceeded recursion
 }
 
-__global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, int max_depth, int rand_seed, camera *d_camera, bvh_node *d_bvh_nodes)
+__global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, int max_depth, int rand_seed, camera *d_camera, bvh_node *d_bvh_nodes, hitable_list *d_lights)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
@@ -88,7 +102,7 @@ __global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, int max_depth, 
         {
             ray r = d_camera->get_ray(i, j, s_i, s_j, recip_sqrt_spp, &local_rand_state);
             // can call vec3.clamp() here but not here because it help with debugging purpose
-            col += get_ray_color_pixel(max_depth, r, d_bvh_nodes, d_camera->background, &local_rand_state);
+            col += get_ray_color_pixel(max_depth, r, d_bvh_nodes, d_lights, d_camera->background, &local_rand_state);
         }
     }
     col /= float(ns);
--- render.h
+++ render.h
@@ -3,4 +3,4 @@
 #include "bvh.h"
 #include "camera.h"
 
-__global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, int max_depth, int rand_seed, camera *d_camera, bvh_node *d_bvh_nodes);
+__global__ void render(vec3 *d_fb, int max_x, int max_y, int ns, int max_depth, int rand_seed, camera *d_camera, bvh_node *d_bvh_nodes, hitable_list *d_lights);
